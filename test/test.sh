61672eb2dd2e53d97b0f56340e84e03ea0b09a99
James Hageman <jamesd@hageman.ca>
2020-01-24 12:53:49 -0500

    test k2

--- test/test.sh
+++ test/test.sh
@@ -2,13 +2,26 @@
 
 set -e
 
+echo "building k1..."
+
 make clean
 make $@ release
 
-echo "building k1..."
-
 echo "running k1..."
-ts7200 bin/choochoos.elf > "test/actual"
-diff --strip-trailing-cr "test/actual" "test/expected"
+ts7200 bin/choochoos.elf > "test/k1.actual"
 
+echo "building k2..."
+
+make clean
+make $@ USER_FOLDER=k2 release
+
+echo "running k2..."
+
+ts7200 bin/choochoos.elf > "test/k2.actual"
+
+echo -n "checking k1: "
+diff --strip-trailing-cr "test/k1.actual" "test/k1.expected"
+echo "outputs match"
+echo -n "checking k2: "
+diff --strip-trailing-cr "test/k2.actual" "test/k2.expected"
 echo "outputs match"
