4e618d2f2182a5d7a7671aa523df7c1bf2e25eef
James Hageman <jamesd@hageman.ca>
2020-01-24 16:45:45 -0500

    still not working, but now we have assert()

--- include/user/syscalls.h
+++ include/user/syscalls.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "vt_escapes.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,6 +14,19 @@ void Yield();
 int Send(int tid, const char* msg, int msglen, char* reply, int rplen);
 int Receive(int* tid, char* msg, int msglen);
 int Reply(int tid, const char* reply, int rplen);
+
+#undef assert
+#define assert(expr)                                                \
+    {                                                               \
+        if (!(expr)) {                                              \
+            bwprintf(COM2,                                          \
+                     VT_RED "[assert:" __FILE__                     \
+                            ":%d tid=%d] Assertion failed: (" #expr \
+                            ")\r\n" VT_NOFMT,                       \
+                     __LINE__, MyTid());                            \
+            Exit();                                                 \
+        }                                                           \
+    }
 
 #ifdef __cplusplus
 }
--- src/user/k2/k2.cc
+++ src/user/k2/k2.cc
@@ -1,11 +1,21 @@
-#include <random>
+#include <cstdlib>
 #include "bwio.h"
 #include "user/syscalls.h"
 
 enum class RPS { NONE = 0, ROCK = 1, PAPER = 2, SCISSORS = 3 };
+enum class Result { DRAW, I_WON, I_LOST };
 
 struct Message {
-    enum { DONE, SIGNUP, SIGNUP_ACK, OUT_OF_SPACE, PLAY } tag;
+    enum {
+        DONE,
+        SIGNUP,
+        SIGNUP_ACK,
+        OUT_OF_SPACE,
+        PLAY,
+        PLAY_RESP,
+        QUIT,
+        OTHER_PLAYER_QUIT,
+    } tag;
     union {
         struct {
         } empty;
@@ -13,6 +23,10 @@ struct Message {
         struct {
             RPS choice;
         } play;
+
+        struct {
+            Result result;
+        } play_resp;
     } data;
 };
 
@@ -58,6 +72,21 @@ struct Game {
             tid2 = tid;
         }
     }
+
+    // returns the tid of the winner, or -1 on a draw
+    int winner() {
+        if (remaining() != 0) {
+            bwprintf(COM2, "winner() called on non-full game\r\n");
+            Exit();
+        }
+        if (choice1 == choice2) {
+            return -1;
+        }
+        // TODO actually follow the game rules
+        return tid1;
+    }
+
+    bool is_over() { return choice1 != RPS::NONE && choice2 != RPS::NONE; }
 };
 
 #define NUM_GAMES 16
@@ -108,9 +137,29 @@ void Server() {
 
             case Message::PLAY: {
                 for (int i = 0; i < NUM_GAMES; i++) {
-                    if (games[i].has(tid)) {
-                        games[i].set(tid, res.data.play.choice);
-                        // TODO if game full, send out results
+                    Game& game = games[i];
+                    if (game.has(tid)) {
+                        game.set(tid, res.data.play.choice);
+                        if (game.is_over()) {
+                            int winner = game.winner();
+                            Result r1 = Result::DRAW;
+                            Result r2 = Result::DRAW;
+                            if (winner == game.tid1) {
+                                r1 = Result::I_WON;
+                                r2 = Result::I_LOST;
+                            } else if (winner == game.tid2) {
+                                r1 = Result::I_LOST;
+                                r2 = Result::I_WON;
+                            }
+                            res = (Message){
+                                .tag = Message::PLAY_RESP,
+                                .data = {.play_resp = {.result = r1}}};
+                            Reply(game.tid1, (char*)&res, sizeof(res));
+                            res.data.play_resp.result = r2;
+                            Reply(game.tid2, (char*)&res, sizeof(res));
+                            game.choice1 = RPS::NONE;
+                            game.choice2 = RPS::NONE;
+                        }
                         break;
                     }
                 }
@@ -121,51 +170,75 @@ void Server() {
                     COM2,
                     "RPSServer: invalid message tag %x, shutting down.\r\n",
                     req.tag);
-                return;
+                Exit();
         }
     }
 }
 
 void Client() {
     int server = 1;  // TODO get this from the name server
+    int my_tid = MyTid();
 
     Message req;
     Message res;
 
     req = (Message){.tag = Message::SIGNUP, .data = {.empty = {}}};
     int code = Send(server, (char*)&req, sizeof(req), (char*)&res, sizeof(res));
-    if (code < 0) {
-        bwprintf(COM2, "tid=%d could not send to RPSServer: %d\r\n", MyTid(),
-                 code);
-        Exit();
-    }
+    assert(code >= 0);
     switch (res.tag) {
         case Message::SIGNUP_ACK:
-            bwprintf(COM2, "tid=%d received signup ack\r\n", MyTid());
+            bwprintf(COM2, "tid=%d received signup ack\r\n", my_tid);
             break;
         default:
-            bwprintf(COM2, "tid=%d received non-ack response %d\r\n", MyTid(),
+            bwprintf(COM2, "tid=%d received non-ack response %d\r\n", my_tid,
                      res.tag);
             Exit();
     }
 
-    std::random_device rd("");  // TODO figure out randomness
-    std::uniform_int_distribution<int> dist(1, 3);
-
     for (int i = 0; i < 4; i++) {
-        RPS choice = (RPS)dist(rd);
+        RPS choice = (RPS)((rand() % 3) + 1);
         req = (Message){.tag = Message::PLAY,
                         .data = {.play = {.choice = choice}}};
-        Send(server, (char*)&req, sizeof(req), (char*)&res, sizeof(res));
+        bwprintf(COM2, "tid=%d sending choice %d\r\n", my_tid, (int)choice);
+        int code =
+            Send(server, (char*)&req, sizeof(req), (char*)&res, sizeof(res));
+        assert(code >= 0);
+
+        switch (res.tag) {
+            case Message::PLAY_RESP: {
+                Result result = res.data.play_resp.result;
+                bwprintf(
+                    COM2, "tid=%d %s\r\n", my_tid,
+                    result == Result::DRAW
+                        ? "draw"
+                        : result == Result::I_WON ? "I won!" : "I lost :(");
+            } break;
+            case Message::OTHER_PLAYER_QUIT: {
+                bwprintf(COM2, "tid=%d other player quit, time to go home\r\n",
+                         my_tid);
+                break;
+            }
+            default: {
+                bwprintf(COM2, "tid=%d invalid reply from server. (tag=%d)\r\n",
+                         my_tid, (int)res.tag);
+                Exit();
+            }
+        }
+
+        req = (Message){.tag = Message::QUIT, .data = {}};
+        bwprintf(COM2, "tid=%d sending quit\r\n", my_tid);
+        code = Send(server, (char*)&req, sizeof(req), (char*)&res, sizeof(res));
+        assert(code >= 0);
     }
 }
 
 }  // namespace rps
 
 void FirstUserTask() {
+    int priorities[3] = {1, 2, 3};
     int server = Create(0, rps::Server);
     (void)server;  // TODO register this with the name server
     for (int i = 0; i < 3; i++) {
-        Create(1, rps::Client);
+        Create(priorities[i % 3], rps::Client);
     }
 }
