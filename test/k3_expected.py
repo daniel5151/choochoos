3398325a4b77c2952003c9b0db6023cb07f53e04
James Hageman <jamesd@hageman.ca>
2020-02-11 13:15:35 -0500

    spawn clockserver notifier before callings RegisterAs
    this ensures that the clockserver notifier is given then next tid after
    the clockserver, instead of potentially allowing other tasks to be
    created while the clockserver is blocked on SRR.

--- test/k3_expected.py
+++ test/k3_expected.py
@@ -1,9 +1,9 @@
 from collections import namedtuple
 
-Task = namedtuple("Task", ["priority", "delay", "num_delays"])
+Task = namedtuple("Task", ["tid", "delay", "num_delays"])
 Event = namedtuple("Event", ["task", "time", "delays_completed"])
 
-tasks = [Task(3, 10, 20), Task(5, 23, 9), Task(6, 33, 6), Task(7, 71, 3)]
+tasks = [Task(4, 10, 20), Task(5, 23, 9), Task(6, 33, 6), Task(7, 71, 3)]
 
 events = [
     Event(task, (i + 1) * task.delay, i + 1)
@@ -12,13 +12,13 @@ events = [
 ]
 
 print("Hello from the choochoos kernel!")
-events.sort(key=lambda evt: [evt.time, -evt.task.priority])
+events.sort(key=lambda evt: [evt.time, -evt.task.tid])
 for event in events:
     print(
         "time=%-3d tid=%d interval=%-2d completed=%2d/%d"
         % (
             event.time,
-            event.task.priority,
+            event.task.tid,
             event.task.delay,
             event.delays_completed,
             event.task.num_delays,
