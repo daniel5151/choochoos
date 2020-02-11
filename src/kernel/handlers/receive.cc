9ccf6450754fc0cd2ddb06e9200123b79251fb17
James Hageman <jamesd@hageman.ca>
2020-02-11 16:17:20 -0500

    inline send and receive helpers

--- src/kernel/handlers/receive.cc
+++ src/kernel/handlers/receive.cc
@@ -4,56 +4,53 @@
 
 namespace kernel::handlers {
 
-static Tid pop_from_send_queue(TaskDescriptor& receiver,
-                               int* sender_tid,
-                               char* recv_buf,
-                               size_t len) {
-    using namespace kernel;
-    kassert(receiver.state.tag == TaskState::READY);
-    kassert(receiver.send_queue_head.has_value());
+int Receive(int* sender_tid, char* msg, int msglen) {
+    kdebug("Called Receive(tid=%p msg=%p msglen=%d)", (void*)sender_tid, msg,
+           msglen);
 
-    kassert(tasks[receiver.send_queue_head.value()].has_value());
+    TaskDescriptor& receiver = tasks[current_task].value();
 
-    TaskDescriptor& sender = tasks[receiver.send_queue_head.value()].value();
-    kassert(sender.state.tag == TaskState::SEND_WAIT);
-
-    size_t n = std::min(sender.state.send_wait.msglen, len);
-    memcpy(recv_buf, sender.state.send_wait.msg, n);
-    *sender_tid = receiver.send_queue_head.value();
-
-    char* reply = sender.state.send_wait.reply;
-    size_t rplen = sender.state.send_wait.rplen;
-    std::optional<Tid> next = sender.state.send_wait.next;
-    sender.state = {.tag = TaskState::REPLY_WAIT, .reply_wait = {reply, rplen}};
-
-    receiver.state = {.tag = TaskState::READY, .ready = {}};
-
-    receiver.send_queue_head = next;
-    if (!receiver.send_queue_head.has_value()) {
-        receiver.send_queue_tail = std::nullopt;
-    } else {
-        kassert(receiver.send_queue_tail.has_value());
-    }
-
-    return n;
-}
-
-int Receive(int* tid, char* msg, int msglen) {
-    kdebug("Called Receive(tid=%p msg=%p msglen=%d)", (void*)tid, msg, msglen);
-
-    TaskDescriptor& task = tasks[current_task].value();
-
-    switch (task.state.tag) {
+    switch (receiver.state.tag) {
         case TaskState::READY: {
-            if (!task.send_queue_head.has_value()) {
-                task.state = {.tag = TaskState::RECV_WAIT,
-                              .recv_wait = {tid, msg, (size_t)msglen}};
+            if (!receiver.send_queue_head.has_value()) {
+                receiver.state = {
+                    .tag = TaskState::RECV_WAIT,
+                    .recv_wait = {sender_tid, msg, (size_t)msglen}};
                 // this will be overwritten when a sender shows up
                 return -3;
             }
 
-            return pop_from_send_queue(task, tid, msg,
-                                       (size_t)std::max(msglen, 0));
+            size_t len = (size_t)std::max(msglen, 0);
+
+            kassert(receiver.state.tag == TaskState::READY);
+            kassert(receiver.send_queue_head.has_value());
+
+            kassert(tasks[receiver.send_queue_head.value()].has_value());
+
+            TaskDescriptor& sender =
+                tasks[receiver.send_queue_head.value()].value();
+            kassert(sender.state.tag == TaskState::SEND_WAIT);
+
+            size_t n = std::min(sender.state.send_wait.msglen, len);
+            memcpy(msg, sender.state.send_wait.msg, n);
+            *sender_tid = receiver.send_queue_head.value();
+
+            char* reply = sender.state.send_wait.reply;
+            size_t rplen = sender.state.send_wait.rplen;
+            std::optional<Tid> next = sender.state.send_wait.next;
+            sender.state = {.tag = TaskState::REPLY_WAIT,
+                            .reply_wait = {reply, rplen}};
+
+            receiver.state = {.tag = TaskState::READY, .ready = {}};
+
+            receiver.send_queue_head = next;
+            if (!receiver.send_queue_head.has_value()) {
+                receiver.send_queue_tail = std::nullopt;
+            } else {
+                kassert(receiver.send_queue_tail.has_value());
+            }
+
+            return n;
         }
         default:
             kdebug("Receive() called from task in non-ready state %d",
--- src/kernel/handlers/send.cc
+++ src/kernel/handlers/send.cc
@@ -4,67 +4,58 @@
 
 namespace kernel::handlers {
 
-static void add_to_send_queue(TaskDescriptor& receiver,
-                              TaskDescriptor& sender,
-                              const char* msg,
-                              size_t msglen,
-                              char* reply,
-                              size_t rplen) {
-    kassert(receiver.state.tag != TaskState::RECV_WAIT);
-    kassert(sender.state.tag == TaskState::READY);
-
-    sender.state = {.tag = TaskState::SEND_WAIT,
-                    .send_wait = {.msg = msg,
-                                  .msglen = msglen,
-                                  .reply = reply,
-                                  .rplen = rplen,
-                                  .next = std::nullopt}};
-    if (!receiver.send_queue_head.has_value()) {
-        kassert(!receiver.send_queue_tail.has_value());
-
-        receiver.send_queue_head = sender.tid;
-        receiver.send_queue_tail = sender.tid;
-    } else {
-        kassert(receiver.send_queue_head.has_value());
-        kassert(receiver.send_queue_tail.has_value());
-
-        kassert(tasks[receiver.send_queue_tail.value()].has_value());
-
-        TaskDescriptor& old_tail =
-            tasks[receiver.send_queue_tail.value()].value();
-        kassert(old_tail.state.tag == TaskState::SEND_WAIT);
-        kassert(!old_tail.state.send_wait.next.has_value());
-        old_tail.state.send_wait.next = sender.tid;
-        receiver.send_queue_tail = sender.tid;
-    }
-}
-
-int Send(
-    int receiver_tid, const char* msg, int msglen, char* reply, int rplen) {
+int Send(int receiver_tid, const char* msg, int mlen, char* reply, int rlen) {
     kdebug("Called Send(tid=%d msg=%p msglen=%d reply=%p rplen=%d)",
            receiver_tid, msg, msglen, reply, rplen);
     if (receiver_tid < 0 || receiver_tid >= MAX_SCHEDULED_TASKS)
         return -1;  // invalid tid
     if (!tasks[receiver_tid].has_value()) return -1;
 
+    size_t msglen = (size_t)std::max(mlen, 0);
+    size_t rplen = (size_t)std::max(rlen, 0);
+
     Tid sender_tid = current_task;
     TaskDescriptor& sender = tasks[sender_tid].value();
     TaskDescriptor& receiver = tasks[receiver_tid].value();
+
     switch (receiver.state.tag) {
         case TaskState::SEND_WAIT:
         case TaskState::REPLY_WAIT:
         case TaskState::READY: {
-            add_to_send_queue(receiver, sender, msg,
-                              (size_t)std::max(msglen, 0), reply,
-                              (size_t)std::max(rplen, 0));
+            kassert(receiver.state.tag != TaskState::RECV_WAIT);
+            kassert(sender.state.tag == TaskState::READY);
+
+            sender.state = {.tag = TaskState::SEND_WAIT,
+                            .send_wait = {.msg = msg,
+                                          .msglen = msglen,
+                                          .reply = reply,
+                                          .rplen = rplen,
+                                          .next = std::nullopt}};
+            if (!receiver.send_queue_head.has_value()) {
+                kassert(!receiver.send_queue_tail.has_value());
+
+                receiver.send_queue_head = sender.tid;
+                receiver.send_queue_tail = sender.tid;
+            } else {
+                kassert(receiver.send_queue_head.has_value());
+                kassert(receiver.send_queue_tail.has_value());
+
+                kassert(tasks[receiver.send_queue_tail.value()].has_value());
+
+                TaskDescriptor& old_tail =
+                    tasks[receiver.send_queue_tail.value()].value();
+                kassert(old_tail.state.tag == TaskState::SEND_WAIT);
+                kassert(!old_tail.state.send_wait.next.has_value());
+                old_tail.state.send_wait.next = sender.tid;
+                receiver.send_queue_tail = sender.tid;
+            }
 
             // the sender should never see this - it should be overwritten
             // by Reply()
             return -4;
         }
         case TaskState::RECV_WAIT: {
-            size_t n = std::min((size_t)std::max(msglen, 0),
-                                receiver.state.recv_wait.len);
+            size_t n = std::min(msglen, receiver.state.recv_wait.len);
             memcpy(receiver.state.recv_wait.recv_buf, msg, n);
             *receiver.state.recv_wait.tid = sender_tid;
 
@@ -76,7 +67,7 @@ int Send(
             TaskDescriptor::write_syscall_return_value(receiver, (int32_t)n);
 
             sender.state = {.tag = TaskState::REPLY_WAIT,
-                            .reply_wait = {reply, (size_t)rplen}};
+                            .reply_wait = {reply, rplen}};
             // the sender should never see this - it should be overwritten
             // by Reply()
             return -3;
