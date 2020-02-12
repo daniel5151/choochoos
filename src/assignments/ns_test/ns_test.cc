e8325b4e5fa8ce1abf0a2d625e1df74d510f1e76
James Hageman <jamesd@hageman.ca>
2020-02-12 14:07:07 -0500

    add extra asserts

--- src/assignments/ns_test/ns_test.cc
+++ src/assignments/ns_test/ns_test.cc
@@ -7,9 +7,16 @@ void T3() { RegisterAs("task 3!!!"); }
 
 void FirstUserTask() {
     int t1 = Create(1, T1);
+
+    // this one is ok
+    assert(WhoIs("Task1") == t1);
+    assert(WhoIs("TASK_2") == -2);
+    assert(WhoIs("task 3!!!") == -2);
+
     int t2 = Create(1, T2);
     int t3 = Create(1, T3);
 
+    // but this one fails
     assert(WhoIs("Task1") == t1);
     assert(WhoIs("TASK_2") == t2);
     assert(WhoIs("task 3!!!") == t3);
