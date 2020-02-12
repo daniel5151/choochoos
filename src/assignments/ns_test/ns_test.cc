ff4139ee8dea025550ba7d1acbbbcae90206f55a
James Hageman <jamesd@hageman.ca>
2020-02-12 13:58:33 -0500

    failing nameserver test

---
+++ src/assignments/ns_test/ns_test.cc
@@ -0,0 +1,18 @@
+#include "user/debug.h"
+#include "user/syscalls.h"
+
+void T1() { RegisterAs("Task1"); }
+void T2() { RegisterAs("TASK_2"); }
+void T3() { RegisterAs("task 3!!!"); }
+
+void FirstUserTask() {
+    int t1 = Create(1, T1);
+    int t2 = Create(1, T2);
+    int t3 = Create(1, T3);
+
+    assert(WhoIs("Task1") == t1);
+    assert(WhoIs("TASK_2") == t2);
+    assert(WhoIs("task 3!!!") == t3);
+
+    bwprintf(COM2, "ok" ENDL);
+}
